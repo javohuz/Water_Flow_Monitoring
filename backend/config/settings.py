632c59f141cce6d5209c104b7ae2ac6b03deb70d
javohuz <javohir52664400456@gmail.com>
2025-04-12 22:47:30 +0500

    🚀 Full project deployed 1.2

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2diz)=jc4jurai%!qw)nx9kg#wrxqf7y#_!&)_9tn_4g^roq!k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
